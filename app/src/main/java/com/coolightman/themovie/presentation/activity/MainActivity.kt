03abc72ff9c2b6b89563055ae4924534809075d9
Coolightman <drugak48@gmail.com>
2021-12-15 18:43:43 +0300

    add binding to mainActivity

--- app/src/main/java/com/coolightman/themovie/presentation/activity/MainActivity.kt
+++ app/src/main/java/com/coolightman/themovie/presentation/activity/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.coolightman.themovie.App
 import com.coolightman.themovie.R
+import com.coolightman.themovie.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
@@ -11,9 +12,12 @@ class MainActivity : AppCompatActivity() {
         (application as App).component
     }
 
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         component.inject(this)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
