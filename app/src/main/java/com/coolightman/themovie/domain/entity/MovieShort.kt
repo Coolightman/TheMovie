af57c3b80400d23602289a852e8b017ebb32581a
Coolightman <drugak48@gmail.com>
2021-12-18 15:19:14 +0300

    delete MovieShort.kt

--- app/src/main/java/com/coolightman/themovie/domain/entity/MovieShort.kt
+++
@@ -1,8 +0,0 @@
-package com.coolightman.themovie.domain.entity
-
-data class MovieShort(
-    val movieId: Long,
-    val rating: String?,
-    val posterPreview: String?,
-    var isFavorite: Boolean
-)
