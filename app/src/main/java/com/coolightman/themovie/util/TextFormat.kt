0882ef319e32fdc7ecbd9256ae367f12953521aa
Coolightman <drugak48@gmail.com>
2022-01-02 17:32:25 +0300

    format percents fix

--- app/src/main/java/com/coolightman/themovie/util/TextFormat.kt
+++ app/src/main/java/com/coolightman/themovie/util/TextFormat.kt
@@ -4,9 +4,9 @@ object TextFormat {
 
     fun formatPercent(rating: String?): String? {
         rating?.let {
-            return if (rating.contains("%")){
+            return if (rating.contains("%") && rating.contains(".")) {
                 "${rating.split(".")[0]}%"
-            } else{
+            } else {
                 rating
             }
         }
