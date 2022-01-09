5b449298cd460a5a8b416e99cfba612ca38f637e
Coolightman <drugak48@gmail.com>
2022-01-09 16:42:27 +0300

    only dark theme

--- app/src/main/java/com/coolightman/themovie/presentation/activity/MainActivity.kt
+++ app/src/main/java/com/coolightman/themovie/presentation/activity/MainActivity.kt
@@ -2,6 +2,7 @@ package com.coolightman.themovie.presentation.activity
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.lifecycle.ViewModelProvider
 import com.coolightman.themovie.App
 import com.coolightman.themovie.R
@@ -14,5 +15,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
     }
 }
