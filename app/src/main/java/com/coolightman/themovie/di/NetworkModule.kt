89d0d944d53e30d31ae6006f322b4d3fc3e2726e
Coolightman <drugak48@gmail.com>
2022-06-05 22:38:41 +0300

    update di

--- app/src/main/java/com/coolightman/themovie/di/NetworkModule.kt
+++ app/src/main/java/com/coolightman/themovie/di/NetworkModule.kt
@@ -7,16 +7,19 @@ import dagger.Provides
 @Module
 class NetworkModule {
 
+    @ApplicationScope
     @Provides
     fun provideApi(): ApiService {
         return ApiClient.getApiService()
     }
 
+    @ApplicationScope
     @Provides
     fun provideApiOld(): ApiServiceOld {
         return ApiClientOld.getApiService()
     }
 
+    @ApplicationScope
     @Provides
     fun provideApiV1(): ApiServiceV1 {
         return ApiClientV1.getApiService()
